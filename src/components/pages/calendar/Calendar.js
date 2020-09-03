d6bec3b85e84e80494beb1c3ca9a78355f50a947
rulyox <email@rulyox.com>
2020-09-03 18:54:47 +0900

    Divided calendar table component

--- src/components/pages/calendar/Calendar.js
+++ src/components/pages/calendar/Calendar.js
@@ -1,79 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './Calendar.css';
 import Header from '../../header/Header';
+import CalendarTable from './table/CalendarTable';
 import Modal from '../../modal/Modal';
 import AddModal from '../../modal/add/AddModal';
 import requests from '../../../requests';
-
-const CalendarTable = (props) => {
-
-    const list = props.data.list;
-    const matrix = props.data.matrix;
-
-    const rows = [];
-    let rowKey = 0;
-
-    if(matrix !== undefined) {
-
-        for(const [index, time] of matrix.entries()) {
-
-            const row = [<th className="calendar__table__time-column" key={rowKey++}>{index}:00</th>];
-
-            for(const item of time) {
-
-                if(item === 0) row.push(<td className="calendar__table__day-column" key={rowKey++}/>);
-                else if(item > 0) {
-
-                    const appointment = list[item-1];
-
-                    const startTime = Number(appointment.startTime.split(':')[0]);
-                    const endTime = Number(appointment.endTime.split(':')[0]);
-
-                    row.push(
-                        <td className="calendar__table__day-column"
-                            key={rowKey++}
-                            rowSpan={endTime-startTime}
-                            style={{'backgroundColor': appointment.color}}>
-                            <span>{appointment.name}</span>
-                            <br />
-                            <span>{appointment.description}</span>
-                        </td>
-                    );
-
-                }
-
-            }
-
-            rows.push(<tr key={rowKey++}>{row}</tr>);
-
-        }
-
-    }
-
-    return (
-        <div id="calendar__table">
-            <table className="table table-bordered">
-                <tbody>
-
-                    <tr>
-                        <th className="calendar__table__time-column"/>
-                        <th className="calendar__table__day-column">Monday</th>
-                        <th className="calendar__table__day-column">Tuesday</th>
-                        <th className="calendar__table__day-column">Wednesday</th>
-                        <th className="calendar__table__day-column">Thursday</th>
-                        <th className="calendar__table__day-column">Friday</th>
-                        <th className="calendar__table__day-column">Saturday</th>
-                        <th className="calendar__table__day-column">Sunday</th>
-                    </tr>
-
-                    {rows}
-
-                </tbody>
-            </table>
-        </div>
-    );
-
-};
 
 const Calendar = ({ match }) => {
 
