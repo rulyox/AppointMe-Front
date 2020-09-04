a6a6b0e16bd40ffb0467467a5ced0f1bcf7e74bf
rulyox <email@rulyox.com>
2020-09-04 22:52:44 +0900

    Added date state

--- src/components/pages/calendar/Calendar.js
+++ src/components/pages/calendar/Calendar.js
@@ -12,7 +12,10 @@ const Calendar = ({ match }) => {
     const [userData, setUserData] = useState({});
     const [appointments, setAppointments] = useState({});
     const [showAddModal, setShowAddModal] = useState(false);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
 
+    // load user data
     useEffect(() => {
 
         requests.user.getData(userId)
@@ -22,14 +25,35 @@ const Calendar = ({ match }) => {
     }, // eslint-disable-next-line react-hooks/exhaustive-deps
     []);
 
+    // calculate week data
     useEffect(() => {
 
-        requests.appointment.get(userId, '20200820')
+        const today = new Date();
+        const weekDay = today.getDay() > 0 ? today.getDay()-1 : 6;
+        const startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - weekDay);
+        const endDate = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + 6);
+
+        setStartDate(startDate);
+        setEndDate(endDate);
+
+    }, // eslint-disable-next-line react-hooks/exhaustive-deps
+    []);
+
+    // get appointments
+    useEffect(() => {
+
+        const year = startDate.getFullYear();
+        const month = startDate.getMonth() + 1;
+        const date = startDate.getDate();
+
+        const week = `${year}${month < 10 ? '0' + month : month}${date < 10 ? '0' + date : date}`;
+
+        requests.appointment.get(userId, week)
             .then((result) => setAppointments(result))
             .catch((error) => console.log(error));
 
     }, // eslint-disable-next-line react-hooks/exhaustive-deps
-    []);
+    [startDate]);
 
     return (
         <div id="calendar">
@@ -40,10 +64,26 @@ const Calendar = ({ match }) => {
 
                 <span id="calendar__top__name">{userData.name}</span>
 
-                <button id="calendar__top__add" type="button" className="btn btn-primary"
-                        onClick={() => setShowAddModal(true)}>
-                    Add
-                </button>
+                <div id="calendar__top__menu">
+
+                    <button type="button" className="btn btn-primary">
+                        <i className="fas fa-chevron-left" />
+                    </button>
+
+                    <span id="calendar__top__date">
+                        {startDate.getFullYear()}-{startDate.getMonth()+1}-{startDate.getDate()} - {endDate.getFullYear()}-{endDate.getMonth()+1}-{endDate.getDate()}
+                    </span>
+
+                    <button type="button" className="btn btn-primary">
+                        <i className="fas fa-chevron-right" />
+                    </button>
+
+                    <button id="calendar__top__add" type="button" className="btn btn-primary"
+                            onClick={() => setShowAddModal(true)}>
+                        Add
+                    </button>
+
+                </div>
 
             </div>
 
