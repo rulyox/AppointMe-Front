66521f5dfd9fa21ce66e9ebdee19896affe192e8
rulyox <email@rulyox.com>
2020-09-06 09:01:11 +0900

    Added handle key press

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -5,6 +5,13 @@ import './Home.css';
 const Home = () => {
 
     const [userId, setUserId] = useState('');
+    const buttonElement = document.getElementById('home__id-button');
+
+    const handleKeyPress = (e) => {
+
+        if(e.key ==='Enter') buttonElement.click();
+
+    };
 
     return (
         <div id="home">
@@ -20,11 +27,12 @@ const Home = () => {
 
                 <div className="home__id-input">
                     <input className="form-control" id="inputId" type="text" placeholder="Enter user ID"
+                           onKeyPress={handleKeyPress}
                            onChange={(e) => setUserId(e.target.value)} />
                 </div>
 
                 <Link to={`/${userId}`}>
-                    <button type="button" className="btn btn-primary">Search</button>
+                    <button type="button" className="btn btn-primary" id="home__id-button">Search</button>
                 </Link>
 
             </div>
